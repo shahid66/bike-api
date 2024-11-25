5e6542138679cf276488ce4704fc02581a0e158a
shahid66 <kk.shahid66@gmail.com>
2024-11-25 22:15:03 +0600

    deployed

--- src/app/middleware/errorHandler.ts
+++ src/app/middleware/errorHandler.ts
@@ -1,12 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import CustomError from '../utils/CustomError'; // Adjust the path as needed
 
-const errorHandler = (
-  err: CustomError,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+const errorHandler = (err: CustomError, req: Request, res: Response) => {
   const statusCode = err.statusCode || 500; // Default to 500 if no statusCode is set
   const message = err.message || 'Internal Server Error';
 
